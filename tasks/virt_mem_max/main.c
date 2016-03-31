01c791da92ac7357a39dc4181a48f3eb7800b6f0
roman <nizshee@gmail.com>
2016-03-31 17:43:13 +0300

    contrl1

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,19 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
 int main()
 {
-    // TODO
+    size_t size = 0;
+    for (size_t i = 1UL << (8 * sizeof(size_t) - 1); i > 0; ) {
+        if (mmap(0, i, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0) != MAP_FAILED) {
+            size += i;
+        } else {
+            i = i >> 1;
+        }
+    }
+    printf("%lu\n", size);
     return 0;
 }
